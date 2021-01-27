741635a173457b5f79b375682f1ab32b8b19ab28
Davide <davidelucifora1@gmail.com>
2021-01-27 18:40:06 +0530

    firebaseconfig.js
    firebase config

---
+++ firebaseconfig.js
@@ -0,0 +1,14 @@
+//  // Your web app's Firebase configuration
+//  var firebaseConfig = {
+//     apiKey: "AIzaSyCCDt2TKK2ZBVg4w6lX8u8vcSbvc1Jfows",
+//     authDomain: "my-library-dd42d.firebaseapp.com",
+//     databaseURL: "https://my-library-dd42d-default-rtdb.firebaseio.com",
+//     projectId: "my-library-dd42d",
+//     storageBucket: "my-library-dd42d.appspot.com",
+//     messagingSenderId: "130814336608",
+//     appId: "1:130814336608:web:2fb9c7c36fbdef7789ed05"
+//   };
+
+//   firebase.initializeApp(firebaseConfig)
+//   const dbRef = firebase.database().ref('Library')
+
